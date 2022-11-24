961c4a0a2db8daa47f9d42de82da24144a315f90
David Gutierrez <gutierrezdvde@gmail.com>
2022-11-24 01:39:00 -0300

    Estructura : Creacion de rama Feature. DG

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Firmas from './componentes/Firmas';
 
+/* ESTA RAMA ES FEATURE */
+
 function App() {
   return (
     <div className="App">
