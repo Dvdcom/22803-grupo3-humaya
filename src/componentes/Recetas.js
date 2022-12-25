2cad66242a164fcd5e38682c830518b6d15dd294
David Gutierrez <gutierrezdvde@gmail.com>
2022-12-25 17:20:32 -0300

    Recetas: se ajusto componente. DG

--- src/componentes/Recetas.js
+++ src/componentes/Recetas.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Swiper, SwiperSlide} from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { FreeMode, Pagination } from "swiper";
 import 'swiper/css';
 import 'swiper/css/free-mode';
@@ -8,7 +8,7 @@ import ProductoCard from "./ProductoCard";
 
 /* para este componente se implemento swiper: https://swiperjs.com/ */
 
-const Recetas = () =>{
+const Recetas = () => {
 
     /* declaro mis hooks de estado */
     const [recetas, setRecetas] = useState([]);
@@ -19,7 +19,7 @@ const Recetas = () =>{
     }, [])
 
     /* creo mi funcion para descargar las recetas */
-    const DescargarRecetas = async() => {
+    const DescargarRecetas = async () => {
 
         /* Aplico un try-catch para la informacion de descarga */
         try {
@@ -35,56 +35,56 @@ const Recetas = () =>{
 
     /* retorno el componente creando un swiper con toda la informacion del estado recetas */
 
-    return(
+    return (
         <>
-        <div className="text-center p-4">
-            <h2 style={{color:"white"}}>Recetas</h2>
-        </div>
-        <div className='Seccion' id='contenedor-recetas'>
-            <div className='swiper-container'>
-                <Swiper
-                freeMode={true}
-                slidesOffsetAfter= {0}
-                pagination={{
-                    clickable:true,
-                }}
-                modules={[FreeMode,Pagination]}
-                className="mySwiper"
-                breakpoints= {{
-                    "@0.00": {
-                    slidesPerGroup:1,
-                    slidesPerView: 1,
-                    spaceBetween: 5
-                    },
-                    "1520": {
-                        slidesPerGroup:2,
-                        slidesPerView: 2,
-                        spaceBetween: 5
-                        }
-                }}
-                style={{
-                    "--swiper-pagination-color": "#63421E",
-                    "--swiper-pagination-bullet-inactive-color": "#bca177",
-                    "--swiper-pagination-bullet-inactive-opacity": "1",
-                    "--swiper-pagination-bullet-size": "10px",
-                    "--swiper-pagination-bullet-horizontal-gap": "3px"
-                }}
-                >
-                <div className="swiper-wrapper">
-                        {recetas.map((element,index)=>(
-                        <SwiperSlide key={index} className="mx-1 d-flex justify-content-center" style={{overflow:"hidden"}}>
-                        <ProductoCard data={{imgSrc:element.imagen,titulo:element.nombre,descripcion: element.descripcion,urlReceta:element.url_receta,url_video:element.video}}/>
-                        </SwiperSlide>
-                        ))}
-                    </div>
-                    <div className="swiper-pagination"></div>
-                    
-                </Swiper>
+            <div className="text-center p-4">
+                <h2 style={{ color: "white" }}>Recetas</h2>
             </div>
-        </div>
+            <div className='Seccion' id='contenedor-recetas'>
+                <div className='swiper-container'>
+                    <Swiper
+                        freeMode={true}
+                        slidesOffsetAfter={0}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        modules={[FreeMode, Pagination]}
+                        className="mySwiper"
+                        breakpoints={{
+                            "@0.00": {
+                                slidesPerGroup: 1,
+                                slidesPerView: 1,
+                                spaceBetween: 5
+                            },
+                            "1520": {
+                                slidesPerGroup: 2,
+                                slidesPerView: 2,
+                                spaceBetween: 5
+                            }
+                        }}
+                        style={{
+                            "--swiper-pagination-color": "#63421E",
+                            "--swiper-pagination-bullet-inactive-color": "#bca177",
+                            "--swiper-pagination-bullet-inactive-opacity": "1",
+                            "--swiper-pagination-bullet-size": "10px",
+                            "--swiper-pagination-bullet-horizontal-gap": "3px"
+                        }}
+                    >
+                        <div className="swiper-wrapper">
+                            {recetas.map((element, index) => (
+                                <SwiperSlide key={index} className="mx-1 d-flex justify-content-center" style={{ overflow: "hidden" }}>
+                                    <ProductoCard data={{ imgSrc: element.imagen, titulo: element.nombre, descripcion: element.descripcion, urlReceta: element.url_receta, url_video: element.video }} />
+                                </SwiperSlide>
+                            ))}
+                        </div>
+                        <div className="swiper-pagination"></div>
+
+                    </Swiper>
+                </div>
+            </div>
         </>
 
     );
 }
-    
-    export default Recetas;
+
+export default Recetas;
