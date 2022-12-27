ce38ebed33f5d7df946fa68bd4460d1085a08d78
David Gutierrez <gutierrezdvde@gmail.com>
2022-12-26 21:36:04 -0300

    Firmas: Se agrega propiedad alt en etiquetas <img>

--- src/componentes/Firmas.js
+++ src/componentes/Firmas.js
@@ -6,47 +6,42 @@ const Firmas = () =>{
             <div className="contenedor-firmas">
                 <img src="/img/git.png" className="log" alt="logo-github"/>
                 <a href="https://github.com/Fedealtrui" className="links link-secondary text-light fw-bold text-decoration-none">
-                    <img className="img-mostrar" src='https://avatars.githubusercontent.com/u/82673183?v=4'/>
+                    <img className="img-mostrar" src='https://avatars.githubusercontent.com/u/82673183?v=4' alt="img-perfil"/>
                     <p className="nombres">Federico Altrui</p> 
                 </a>
                 <img src="/img/git.png" className="log" alt="logo-github"/>
                 <a href="https://github.com/gissestephy" className="links link-secondary text-light fw-bold text-decoration-none">
-                    <img className="img-mostrar" src='https://avatars.githubusercontent.com/u/103209971?v=4'/>
+                    <img className="img-mostrar" src='https://avatars.githubusercontent.com/u/103209971?v=4' alt="img-perfil"/>
                     <p className="nombres">Gisela Colmeiro</p>
                 </a>
                 <img src="/img/git.png" className="log" alt="logo-github"/>
                 <a href="https://github.com/JuanGabrielChaves" className="links link-secondary text-light fw-bold text-decoration-none">
-                    <img className="img-mostrar" src='https://avatars.githubusercontent.com/u/88752030?v=4'/>
+                    <img className="img-mostrar" src='https://avatars.githubusercontent.com/u/88752030?v=4' alt="img-perfil"/>
                     <p className="nombres">Gabriel Chaves</p>
                 </a>
                 <img src="/img/git.png" className="log" alt="logo-github"/>
                 <a href="https://github.com/sofiamfernandez" className="links link-secondary text-light fw-bold text-decoration-none">
-                    <img className="img-mostrar" src='https://avatars.githubusercontent.com/u/68400669?v=4'/>
+                    <img className="img-mostrar" src='https://avatars.githubusercontent.com/u/68400669?v=4' alt="img-perfil"/>
                     <p className="nombres">Sofia Fernandez</p>
                 </a>
                 <img src="/img/git.png" className="log" alt="logo-github"/>
                 <a href="https://github.com/Dvdcom" className="links link-secondary text-light fw-bold text-decoration-none">
-                    <img className="img-mostrar" src='https://avatars.githubusercontent.com/u/105727909?v=4'/>
+                    <img className="img-mostrar" src='https://avatars.githubusercontent.com/u/105727909?v=4'alt="img-perfil"/>
                     <p className="nombres">David Gutierrez</p>
                 </a>
                 <img src="/img/git.png" className="log" alt="logo-github"/>
                 <a href="https://github.com/daianaryan" className="links link-secondary text-light fw-bold text-decoration-none">
-                    <img className="img-mostrar" src='https://avatars.githubusercontent.com/u/88803959?v=4'/>
+                    <img className="img-mostrar" src='https://avatars.githubusercontent.com/u/88803959?v=4' alt="img-perfil"/>
                     <p className="nombres">Daiana Ryan</p>
                 </a>
                 <img src="/img/git.png" className="log" alt="logo-github"/>
-                <a href="https://github.com/Fragusan" className="links link-secondary text-light fw-bold text-decoration-none">
-                    <img className="img-mostrar" src='https://avatars.githubusercontent.com/u/78457501?v=4'/>
-                    <p className="nombres">Franco Sánchez</p>
-                </a>
-                <img src="/img/git.png" className="log" alt="logo-github"/>
                 <a href="https://github.com/GerardJV" className="links link-secondary text-light fw-bold text-decoration-none">
-                    <img className="img-mostrar" src='https://avatars.githubusercontent.com/u/90412515?v=4'/>
+                    <img className="img-mostrar" src='https://avatars.githubusercontent.com/u/90412515?v=4' alt="img-perfil"/>
                     <p className="nombres">Gerardo Vargas</p>
                 </a>
                 <img src="/img/git.png" className="log" alt="logo-github"/>
                 <a href="https://github.com/raquelzaballia" className="links link-secondary text-light fw-bold text-decoration-none">
-                    <img className="img-mostrar" src='https://avatars.githubusercontent.com/u/92328004?v=4'/>
+                    <img className="img-mostrar" src='https://avatars.githubusercontent.com/u/92328004?v=4' alt="img-perfil"/>
                     <p className="nombres">Carmen Zaballia</p>
                 </a>
             </div>
