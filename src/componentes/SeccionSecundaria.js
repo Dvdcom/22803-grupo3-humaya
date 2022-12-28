2a30fb5129d07bbaf42de525f2831c8cdd311967
David Gutierrez <gutierrezdvde@gmail.com>
2022-12-28 12:26:27 -0300

    FIX se agrega propiedad alt a las imagenes

--- src/componentes/SeccionSecundaria.js
+++ src/componentes/SeccionSecundaria.js
@@ -4,7 +4,7 @@ const SeccionSecundaria = () =>{
         <div className='Seccion-fondo bg-gradient-dark-brown py-4' id='SeccionSecundaria'>
             <h1 className="text-center fw-bold text-light animate__animated animate__pulse">La mejor calidad solo para vos</h1>
             <div className="calidadiso">
-                    <img src="../img/Calidad.png" height="260px"/>
+                    <img src="../img/Calidad.png" height="260px" alt="img"/>
             </div>   
         </div>
         
