bce44fde1f5be3eddf9109bbb6be1380a2a1a153
David Gutierrez <gutierrezdvde@gmail.com>
2022-12-29 10:16:41 -0300

    Encabezado: se ajusta el sweetAlert. DG

--- src/componentes/Encabezado.js
+++ src/componentes/Encabezado.js
@@ -4,7 +4,7 @@ import Swal from 'sweetalert2'
 const Encabezado = () =>{
 
     Swal.fire({
-        title: 'Alfajor Sabroson!',
+        title: "<img src='https://i.postimg.cc/fysypQ8G/Logo-Humaya100x100.png' heigth='50px' width='50px' alt='humaya-logo'></img>  Alfajor Sabroson!",
         html: '<b>50% de descuento</b> en todas tus compras',
         imageUrl: 'http://supermercadoscomodin.com/wp-content/uploads/2019/11/Supermercados-comodin-receta-entrada-34.jpg',
         imageWidth: 450,
@@ -13,8 +13,10 @@ const Encabezado = () =>{
         position: 'center',
         confirmButtonText: "Comprar",
         confirmButtonColor: "#63421e",
-        borderButtonColor: "rgba(211, 170, 93, 0.233)",
-      });
+        borderButtonColor: "rgba(188, 161, 119, 0.233)",
+        background:"#63421e",
+        color:'White',
+    });
 
     return(
         <div className="img-fluid m-0 p-0" id="portada">
