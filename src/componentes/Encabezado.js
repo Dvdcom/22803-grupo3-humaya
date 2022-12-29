80c904314633e73e02db8e758242168151b854ae
gissestephy <gisela.s.colmeiro@gmail.com>
2022-12-29 09:00:50 -0300

    se agrego sweetalert

--- src/componentes/Encabezado.js
+++ src/componentes/Encabezado.js
@@ -1,10 +1,31 @@
+import Swal from 'sweetalert2'
+
 const Encabezado = () =>{
 
+    Swal.fire({
+        title: 'Alfajor Sabroson!',
+        html: '<b>50% de descuento</b> en todas tus compras',
+        imageUrl: 'http://supermercadoscomodin.com/wp-content/uploads/2019/11/Supermercados-comodin-receta-entrada-34.jpg',
+        imageWidth: 450,
+        imageHeight: 300,
+        imageAlt: 'Alfajores de dulce de leche',
+        position: 'center',
+        confirmButtonText: "Comprar",
+        confirmButtonColor: "#63421e",
+        borderButtonColor: "rgba(211, 170, 93, 0.233)",
+      });
+
     return(
-        <div className='Seccion Seccion-fondo' id='Encabezado Seccion'>
-            <h1>ENCABEZADO PRINCIPAL</h1>
+        <div className="img-fluid m-0 p-0" id="portada">
+            <div className="card mt-0 mx-2 pt-4 fondoTransparente inicio">
+                <div className="card-body align-items-center my-auto mt-5 container-text">
+                    <h5 className="card-title text-white tituloPresentacion">DULCES DEL ALMA</h5>
+                    <p className="card-text mt-3 section-desktop">Los productos HUMAYA son elaborados desde hace más de 50 años fiel a las tradiciones familiares y sabores caseros, acompañándote en los momentos más importantes de la vida.</p>
+                    <button href="#" type="button" className="btn btn-dark d-grid gap-2 col-8 mx-auto mt-4" id="btn-presentacion">CONOCENOS</button>
+                </div>
+            </div>
         </div>
     );
 }
     
-    export default Encabezado;
+export default Encabezado;
