138bfc2fdb8dccc44479317756195eb3dfa69377
David Gutierrez <gutierrezdvde@gmail.com>
2022-12-29 09:29:56 -0300

    Desbordamiento: se ajusto css. DG

--- src/componentes/SeccionSecundaria.js
+++ src/componentes/SeccionSecundaria.js
@@ -1,7 +1,7 @@
 const SeccionSecundaria = () =>{
 
     return(
-        <div className='Seccion-fondo bg-gradient-dark-brown py-4' id='SeccionSecundaria'>
+        <div className='py-4' id='SeccionSecundaria'>
             <h1 className="text-center fw-bold text-light animate__animated animate__pulse">La mejor calidad solo para vos</h1>
             <div className="calidadiso">
                     <img src="../img/Calidad.png" height="210px" alt="img"/>
